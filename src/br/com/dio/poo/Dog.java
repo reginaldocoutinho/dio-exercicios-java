dcb23f46b61c648e7043652695b519121d84e954
reginaldocoutinho <reginaldo_re12@hotmail.com>
2025-10-16 11:51:32 -0300

    Criado um método construtor record, ele é um método compacto onde os objetos são imutaveis

---
+++ src/br/com/dio/poo/Dog.java
@@ -0,0 +1,5 @@
+package br.com.dio.poo;
+
+public record Dog(String name, String act , String owner) {
+//record é uma forma de trabalhar com objetos imutaveis
+}
--- src/br/com/dio/poo/Main.java
+++ src/br/com/dio/poo/Main.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Person.setAct("study");
         var stepson = new Person("João");
         stepson.setAge(14);
+
         var stepdaughter = new Person("Sofia");
         stepdaughter.setAge(12);
+
         System.out.printf("My stepchildren "+stepson.getName()+" and "+stepdaughter.getName()
         +". "+stepson.getName()+" is "+stepson.getAge()+" years old and "
-                +stepdaughter.getName()+" is "+stepdaughter.getAge() +" years old. They "+ Person.getAct() +" during the week");
+                +stepdaughter.getName()+" is "+stepdaughter.getAge() +" years old. They "+ Person.getAct() +" during the week.\n");
+
+        var lupita = new Dog("Lupita","Sleeping", "Mikaeli");
+        var zuri = new Dog("Zuri","Barking", "Sofia");
+
+        System.out.printf(zuri.owner()+" is "+zuri.name()+"`s owner\n"+
+                lupita.owner() +" is "+ lupita.name()+"`s owner");
     }
 }
