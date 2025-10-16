c2b402090733752782d53353be293bf2934e3b68
reginaldocoutinho <reginaldo_re12@hotmail.com>
2025-10-16 11:20:57 -0300

    Adicionado novos exemplos como métodos construtores

--- src/br/com/dio/poo/Main.java
+++ src/br/com/dio/poo/Main.java
@@ -1,4 +1,16 @@
 package br.com.dio.poo;
-
+import java.util.Scanner;
 public class Main {
+
+    public static void main(String[] args) {
+
+        Person.setAct("study");
+        var stepson = new Person("João");
+        stepson.setAge(14);
+        var stepdaughter = new Person("Sofia");
+        stepdaughter.setAge(12);
+        System.out.printf("My stepchildren "+stepson.getName()+" and "+stepdaughter.getName()
+        +". "+stepson.getName()+" is "+stepson.getAge()+" years old and "
+                +stepdaughter.getName()+" is "+stepdaughter.getAge() +" years old. They "+ Person.getAct() +" during the week");
+    }
 }
--- src/br/com/dio/poo/Person.java
+++ src/br/com/dio/poo/Person.java
@@ -1,4 +1,34 @@
 package br.com.dio.poo;
 
 public class Person {
+
+    private String name;
+    private int age;
+    private static String act;
+
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public static void setAct(String active){
+        act = active;
+    }
+
+    public static String getAct(){
+        return act;
+    }
+
 }
