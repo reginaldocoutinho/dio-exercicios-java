a189fc8686c2d19db05d276802ded937d88c57f7
reginaldocoutinho <reginaldo_re12@hotmail.com>
2025-10-22 11:38:19 -0300

    Criado a Classe Maquina de Banho com todos os atributos e funcionalidades requisitadas no exercicio 3

--- src/br/com/dio/petshop/MaquinaDeBanho.java
+++ src/br/com/dio/petshop/MaquinaDeBanho.java
@@ -1,5 +1,76 @@
 package br.com.dio.petshop;
 
-public class Petshop {
-    
+public class MaquinaDeBanho {
+    private double nivelAgua = 0;
+    private double nivelShampoo = 0;
+    private double nivelMaxAgua = 30;
+    private double nivelMaxShampoo = 10;
+    private boolean flagMaquinaOcupada = false;
+    private boolean flagMaquinaSuja = false;
+    private boolean flagEmUso = false;
+
+    public boolean getFlagEmUso(){
+        return flagEmUso;
+    }
+
+    public boolean getFlagMaquinaOcupada(){
+        return flagMaquinaOcupada;
+    }
+    public boolean getFlagMaquinaSuja(){
+        return flagMaquinaSuja;
+    }
+    public double getNivelAgua(){
+        return nivelAgua;
+    }
+    public double getNivelShampoo(){
+        return nivelShampoo;
+    }
+
+    public void setFlagEmUso(boolean valor){
+        this.flagEmUso = valor;
+    }
+
+    public void setNivelAgua(double valor){
+        if(nivelAgua < nivelMaxAgua){
+            System.out.println("Você colocou 2l de água.");
+            this.nivelAgua += valor;
+        }else{
+            System.out.println("Reservatório de água está cheio.");
+        }
+
+    }
+
+    public void setNivelShampoo(double valor){
+        if(nivelShampoo < nivelMaxShampoo){
+            System.out.println("Voce colocou 2l, serve para um banho.");
+            this.nivelShampoo += valor;
+        }else{
+            System.out.println("Reservatório de shampoo está cheio.");
+        }
+
+    }
+
+    public void setFlagMaquinaOcupada(boolean flagMaquinaOcupada) {
+        this.flagMaquinaOcupada = flagMaquinaOcupada;
+    }
+
+    public void setFlagMaquinaSuja(boolean flagMaquinaSuja){
+        this.flagMaquinaSuja = flagMaquinaSuja;
+    }
+
+
+    public void darBanho(double addnivelAgua, double addnivelShampoo ){
+        setFlagMaquinaOcupada(true);
+        setFlagMaquinaSuja(true);
+        nivelAgua = addnivelAgua - 10;
+        nivelShampoo = addnivelShampoo - 2;
+        flagMaquinaSuja = true;
+    }
+
+    public void limparMaquina(double nivelAgua, double nivelShampoo){
+        this.nivelShampoo = nivelShampoo - 1;
+        this.nivelAgua = nivelAgua - 3;
+        flagMaquinaSuja = false;
+    }
+
 }
--- src/br/com/dio/petshop/PetShop.java
+++ src/br/com/dio/petshop/PetShop.java
@@ -1,4 +1,106 @@
 package br.com.dio.petshop;
 
-public class Main {
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class PetShop {
+
+    public static void main(String[] args) {
+        var entrada = new Scanner(System.in);
+        var maquinaDeBanho = new MaquinaDeBanho();
+        System.out.println("**Maquina de banho**");
+        System.out.println("====================");
+        System.out.println("ligada maquina?(s/n)");
+        var ligada = entrada.next();
+        if(ligada.equalsIgnoreCase("s")){
+            while(ligada.equalsIgnoreCase("s")){
+                var flagEmUso = maquinaDeBanho.getFlagEmUso() ? "sim" : "nao";
+                var nivelAgua = maquinaDeBanho.getNivelAgua();
+                var nivelShampoo = maquinaDeBanho.getNivelShampoo();
+                var maquinaSuja = maquinaDeBanho.getFlagMaquinaSuja() ? "sim" : "não";
+                System.out.println("\nStatus de uso:" +
+                        "\nEm uso: "+flagEmUso +
+                        "\nNível Agua: "+ nivelAgua +
+                        "\nNível Shampoo: "+ nivelShampoo +
+                        "\nMaquina suja? "+ maquinaSuja +
+                        "\n");
+                System.out.println("\n0 -Desligar Maquina" +
+                        "\n1 - Colocar Pet" +
+                        "\n2 - Retirar Pet" +
+                        "\n3 - Dar Banho" +
+                        "\n4 - Repor Água" +
+                        "\n5 - Repor Shampoo" +
+                        "\n6 - Limpar Maquina"+
+                        "\nSelecione umas das opções acima");
+                var options = entrada.nextInt();
+                switch (options){
+                    case 0:
+                        if(flagEmUso.equalsIgnoreCase("sim")){
+                            System.out.println("Retire o pet da maquina!");
+                        }else if(maquinaDeBanho.getFlagMaquinaSuja() == true){
+                            System.out.println("Maquina suja limpe ela!");
+                        }else{
+                            ligada = "n";
+                            System.out.println("Você parou o processo!");
+                        }
+                        break;
+                    case 1:
+                        if(flagEmUso.equalsIgnoreCase("sim")){
+                            System.out.println("Maquina já está em uso," +
+                                    "\n verifique se o banho terminou e retire o pet");
+                        }
+                        maquinaDeBanho.setFlagEmUso(true);
+                        System.out.println("Colocou um pet na maquina!");
+                        break;
+                    case 2:
+                        if(flagEmUso.equalsIgnoreCase("nao")){
+                            System.out.println("Não tem nenhum pet utilizando a maquina!");
+                        }else {
+                            maquinaDeBanho.setFlagEmUso(false);
+                            System.out.println("Retirou o pet da maquina!");
+                            if(maquinaDeBanho.getFlagMaquinaSuja() == true){
+                                System.out.println("Maquina suja limpe ela!");
+                            }
+                        }
+                        break;
+                    case 3:
+                        if(flagEmUso.equalsIgnoreCase("sim")){
+                            System.out.println("Dar banho?(s/n)");
+                            var escolhaBanho = entrada.next();
+                            if(escolhaBanho.equalsIgnoreCase("sim") || escolhaBanho.equalsIgnoreCase("s")){
+                                if(nivelAgua < 10){
+                                    System.out.println("Maquina não possui agua suficiente!\nReponha a agua!");
+                                }else if(nivelShampoo < 2){
+                                    System.out.println("Maquina não possui shampoo suficiente!\nReponha o shampoo!");
+                                }else{
+                                    maquinaDeBanho.darBanho(nivelAgua,nivelShampoo);
+                                    System.out.println("Chegou");
+                                }
+                            }else if(escolhaBanho.equalsIgnoreCase("não") || escolhaBanho.equalsIgnoreCase("n")){
+                                System.out.println("\nEscolha outra opção\n");
+                            }
+                        }else{
+                            System.out.println("Coloque um pet dentro da maquina para iniciar o processo!");
+                        }
+                        break;
+                    case 4:
+                        maquinaDeBanho.setNivelAgua(2);
+                        break;
+                    case 5:
+                        maquinaDeBanho.setNivelShampoo(2);
+                        break;
+                    case 6:
+                        maquinaDeBanho.limparMaquina(nivelAgua,nivelShampoo);
+                        break;
+                    default:
+                        System.out.println("Opção inválida!");
+                }
+            }
+        }else if(ligada.equalsIgnoreCase("n")){
+            System.out.println("\nMaquina continua desligada!");
+        }else{
+            System.out.println("Opção inválida!");
+        }
+
+    }
 }
