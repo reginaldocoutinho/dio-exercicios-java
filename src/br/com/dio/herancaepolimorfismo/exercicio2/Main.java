ba7c75e2b915ef328448bb73adeb228b1850a0c8
reginaldocoutinho <reginaldo_re12@hotmail.com>
2025-11-02 07:58:20 -0300

    Corrigido os parametros do método estático de entrada

--- src/br/com/dio/herancaepolimorfismo/exercicio2/Main.java
+++ src/br/com/dio/herancaepolimorfismo/exercicio2/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         var entrada = new Scanner(System.in);
         Colaborador gerente = new Gerente();
         Colaborador vendedor = new Vendedor();
